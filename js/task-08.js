9b7156b37b7ebf35b94feca0ccc00c4dad93d1ef
SENKOVAOLGA <senkovaom320@ukr.net>
2023-10-03 21:29:58 +0300

    update task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,18 +1,19 @@
 const formEl = document.querySelector(".login-form");
-formEl.addEventListener('submit',formSubmit)
-function formSubmit(e) {
-    e.preventDefault()
+formEl.addEventListener('submit', formSubmit);
 
-    const { email, password } = e.currentTarget.elements
-    console.log(email.value)
-    console.log(password.value)
-    const data = {
-        email: email.value,
-        password: password.value,
-    };
-    console.log(data);
+function formSubmit(e) {
+    e.preventDefault();
+
+    const emailInput = e.currentTarget.elements.email;
+    const passwordInput = e.currentTarget.elements.password;
+
+    const email = emailInput.value;
+    const password = passwordInput.value;
+
+    console.log("Email:", email);
+    console.log("Password:", password);
+
     if (email === '' || password === '') {
         alert('Всі поля повинні бути заповнені');
-    };
-
+    } 
 }
